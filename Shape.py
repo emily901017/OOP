427828212f62f9888ccb796e26293f4254264f7a
emily901017 <taeny1017@gmail.com>
2025-07-22 23:01:56 +0800

    add Shape class

--- Shape.py
+++ Shape.py
@@ -8,5 +8,50 @@
 # 撰寫函式接收 Shape，統一呼叫方法計算並輸出
 # 目標：抽象設計、方法覆寫、多型應用
 
-class Shape:
+from abc import ABC, abstractmethod
+import math 
+
+class Shape(ABC):
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    def get_name(self):
+        return self.__class__.__name__ #self.__class__：這個物件所屬的類別，self.__class__.__name__：該類別的名稱
+
+    def __str__(self):
+        return f"{self.get_name()}面積為：{self.get_area()}"
+
+class Circle(Shape):
+    #name = "圓型" 若想把名稱改為中文可以加這行
+    def __init__(self,radius):
+        self.radius = radius
+
+    def get_area(self):
+        return math.pi * self.radius **2 
     
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        self.width = width 
+        self.height = height 
+
+    def get_area(self):
+        return self.width * self.height
+      
+    
+class Triangle(Shape):
+    def __init__(self,base,height):
+        self.base = base
+        self.height = height
+      
+
+    def get_area(self):
+        return self.base * self.height /2 
+    
+Shape1 = Circle(5)
+Shape2 = Rectangle(2,4)
+Shape3 = Triangle(6,8)
+
+print(Shape1)
+print(Shape2)
+print(Shape3)
